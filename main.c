b0d56b96ae90b67eac1a3bdd60d87d1368ad3adc
Wolfgang Poppelreuter <wolfgang@poppelreuter.de>
2016-03-28 22:47:04 +0200

    better testings of bind port, using "/etc/services" to mapping name to port number

--- main.c
+++ main.c
@@ -25,6 +25,9 @@ along with csslh.  If not, see <http://www.gnu.org/licenses/>.
 #include <sys/socket.h>
 #include <netdb.h>
 #include <errno.h>
+#include <ctype.h>
+#include <netdb.h>
+#include <syslog.h>
 
 #include "config.h"
 #include "utils.h"
@@ -36,9 +39,50 @@ int main(int argc, char* argv[])
 {
 	int rc = parseCommandLine(argc, argv);
 	
+	// resolv port to numeric
+	int publicPort;
+	
+	if(isdigit(*(pGetConfig()->publicPort)))
+	{
+		char* ptr = pGetConfig()->publicPort;
+		
+		while(*ptr)
+		{
+			if(!isdigit(*ptr))
+			{
+				fprintf(stderr,
+					"error: Portnumber contains an non digit\n");
+				return(EINVAL);
+			}
+			ptr++;
+		}
+		
+		publicPort = atoi(pGetConfig()->publicPort);
+	}
+	else
+	{
+		// resolve service
+		struct servent* resolvPort = getservbyname(pGetConfig()->publicPort, "tcp");
+		
+		if(resolvPort == NULL)
+		{
+			fprintf(stderr,
+				"error: unable to map service %s to port number\n",
+				pGetConfig()->publicPort);
+				
+			return(EINVAL);
+		}
+		
+		publicPort = ntohs(resolvPort->s_port);
+	
+		syslog(LOG_DEBUG, "public port = %s -> %d\n", pGetConfig()->publicPort, publicPort);
+		
+		endservent();
+	}
+	
 	// test for root
 	if(0 == geteuid() || // need to be root
-	   atoi(pGetConfig()->publicPort) >= 1024)
+	   publicPort >= 1024)
 	{   
 	  struct addrinfo* addrInfo;
 	  struct addrinfo* addrInfoBase;
@@ -47,7 +91,6 @@ int main(int argc, char* argv[])
 
 	  resolvAddress(pGetConfig()->publicHostname, pGetConfig()->publicPort,
 			&addrInfoBase);
-	  
 	  
           addrInfo = addrInfoBase;
 
@@ -98,8 +141,8 @@ int main(int argc, char* argv[])
 	} // serverSocket
 	else
 	  {
-		fprintf(stderr,"%s must be started as root\n\r",
-			argv[0]);
+		fprintf(stderr,"%s must be started as root to use port %s\n",
+			argv[0], pGetConfig()->publicPort);
 		
 		rc = EACCES;
 	  }
